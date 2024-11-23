1a33696e53b6d095a4afa5ce6b82fbcd8afc7ffb
kharakkular <kharakkular@gmail.com>
2024-11-22 21:11:35 -0500

    added code for handleHttpRequestMethodNotSupported in globalExceptionHandler

--- src/main/java/com/springboot/todo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/springboot/todo/exception/GlobalExceptionHandler.java
@@ -2,14 +2,19 @@ package com.springboot.todo.exception;
 
 import java.util.Date;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.springboot.todo.payload.ErrorDetails;
 
+@ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
 	
 	@ExceptionHandler(ResourceNotFoundException.class)
@@ -26,5 +31,12 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
 		ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), request.getDescription(false));
 		
 		return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@Override
+	public ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request){
+		ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity<>(errorDetails, HttpStatusCode.valueOf(status.value()));
 	}
 }
