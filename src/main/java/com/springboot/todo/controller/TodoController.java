d4deb0d0b4614ec8a2e0019e4c6637ca4620e18f
kharakkular <kharakkular@gmail.com>
2024-11-29 16:58:41 -0500

    Added code for handling validation logic in createTodo method

--- src/main/java/com/springboot/todo/controller/TodoController.java
+++ src/main/java/com/springboot/todo/controller/TodoController.java
@@ -1,11 +1,21 @@
 package com.springboot.todo.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.modelmapper.spi.ErrorMessage;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -14,9 +24,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springboot.todo.exception.ValidationException;
 import com.springboot.todo.payload.PaginatedResponse;
 import com.springboot.todo.payload.TodoDto;
 import com.springboot.todo.service.TodoService;
+import com.springboot.todo.validation.TodoValidator;
 
 @RestController
 @RequestMapping("/api/todos")
@@ -24,12 +36,22 @@ public class TodoController {
 	
 	private TodoService todoService;
 	
-	public TodoController(TodoService _service) {
+	private TodoValidator validator;
+	
+	public TodoController(TodoService _service, TodoValidator _validator) {
 		todoService = _service;
+		validator = _validator;
 	}
 	
 	@PostMapping
-	public ResponseEntity<TodoDto> createTodo(@RequestBody TodoDto todoDto) {
+	public ResponseEntity<TodoDto> createTodo(@RequestBody TodoDto todoDto, BindingResult bindingResult) {
+		validator.validate(todoDto, bindingResult);
+		
+		if(bindingResult.hasErrors()) {
+			List<String> errorMessages = bindingResult.getAllErrors().stream().map(obj -> obj.getDefaultMessage()).collect(Collectors.toList());
+			
+			throw new ValidationException(errorMessages);
+		}
 		return new ResponseEntity<TodoDto>(todoService.createTodo(todoDto), HttpStatus.CREATED);
 	}
 	
